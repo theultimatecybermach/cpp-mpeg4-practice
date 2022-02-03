ee719d0dd777e52105d26b8f3f19081b99b2eac3
yfuoft <yousuf.farhan@mail.utoronto.ca>
2022-02-03 11:36:45 -0500

    Added Rendering
    Fixed the sample data.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argv, const char** argc){
     GLFWwindow* window;
-    unsigned char* data = new unsigned char[200*200*3];
+    unsigned char* data = new unsigned char[500*500*3];
     if (!glfwInit()){
         printf("GLFW could not be intiailized.");
         return 1;
@@ -14,11 +14,11 @@ int main(int argv, const char** argc){
         return 1;
     }
     
-    for(int y=0; y<200; ++y){
-        for (int x=0; x<200; ++x){ 
-            data[y * 200 * 3 + x * 3 ] = 00xff;
-            data[y * 200 * 3 + x * 3 + 1 ] = 00x00;
-            data[y * 200 * 3 + x *3 + 2] = 00x00;
+    for(int y=0; y<500; ++y){
+        for (int x=0; x<500; ++x){ 
+            data[y * 500 * 3 + x * 3 ] = 0xff;
+            data[y * 500 * 3 + x * 3 + 1 ] = 0x00;
+            data[y * 500 * 3 + x *3 + 2] = 0x00;
 
         }
     }
@@ -27,7 +27,7 @@ int main(int argv, const char** argc){
 
     while (!glfwWindowShouldClose(window)){
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glDrawPixels(200,200,GL_RGB, GL_UNSIGNED_BYTE,data);
+        glDrawPixels(500,500,GL_RGB, GL_UNSIGNED_BYTE,data);
         glfwSwapBuffers(window);
         glfwWaitEvents();
 
